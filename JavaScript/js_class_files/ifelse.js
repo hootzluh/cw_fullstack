e5d7c974d9fe80f00959ad27554956b5d5ed4035
Justin Hutzler <hootzluh@gmail.com>
2022-12-13 01:06:04 -0600

    Added JS Assignment files.

---
+++ JavaScript/js_class_files/ifelse.js
@@ -0,0 +1,36 @@
+// const age = +prompt("How old are you?");
+
+// console.log(age);
+
+// if (age > 25) {
+//   console.log("You are allowed...");
+// } else {
+//   console.log("You are not allowed...");
+// }
+
+// const num = +prompt("Enter a number:");
+// if (num % 2 == 0) {
+//   console.log("The number is even.");
+// } else {
+//   ("The number is odd");
+// }
+
+// const number1 = +prompt("Enter first number");
+// const number2 = +prompt("Enter second number");
+
+// if (number1 > number2) {
+//   console.log("number1 is greater");
+// } else if (number2 > number1) {
+//   console.log("number2 is greater");
+// } else {
+//   console.log("The numbers are the same");
+// }
+
+
+// --------------------------------------------------------
+// Ternary IF statements
+
+const score = +prompt('Enter your score');
+let grade = (score >= 70) ? 'passing' : 'failing';
+
+console.log(`Your score is ${grade}`);
