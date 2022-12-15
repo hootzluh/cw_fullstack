d2850fd04d448495d57976d6f255fe0159034929
Justin Hutzler <hootzluh@gmail.com>
2022-12-15 11:18:48 -0600

    JS Loops Assignment.

--- JavaScript/js_assignments/js_assignment12_13_22.js
+++ JavaScript/js_assignments/js_assignment12_13_22.js
@@ -1,42 +1,51 @@
 // JS Assignment 01
-const num1 = +prompt("Enter a number:");
+const num = +prompt('Enter a number: ');
+const diff = (num - 20);
 
-if (num1 >= 50 && num1 <= 100) {
-  console.log(`${num1} is withing the range of 50 to 100`);
-} else {
-  console.log(`${num1} is not within the range of 50 to 100`);
+if (diff <= 20){
+  console.log(diff);
+} else if (diff > 20){
+  console.log(20 - diff);
 }
+
 
 //JS Assignment 02
-const first_num = +prompt(
-  "Pick three different numbers. Enter the first number: "
-);
-const second_num = +prompt("Enter the second number: ");
-const third_num = +prompt("Enter the third number: ");
+// const first_num = +prompt(
+//   "Pick three different numbers. Enter the first number: "
+// );
+// const second_num = +prompt("Enter the second number: ");
+// const third_num = +prompt("Enter the third number: ");
 
-if (first_num > second_num && first_num > third_num) {
-  console.log(`${first_num} is the largest number.`);
-} else if (second_num > first_num && second_num > third_num) {
-  console.log(`${second_num} is the largest number.`);
-} else if (third_num > first_num && third_num > second_num) {
-  console.log(`${third_num} is the largest number.`);
-}
+// if (first_num > second_num && first_num > third_num) {
+//   console.log(`${first_num} is the largest number.`);
+// } else if (second_num > first_num && second_num > third_num) {
+//   console.log(`${second_num} is the largest number.`);
+// } else if (third_num > first_num && third_num > second_num) {
+//   console.log(`${third_num} is the largest number.`);
+// }
 
 //JS Assignment 03
-const num01 = +prompt(
-  "Will the sign be + or - ? Pick three numbers. Enter the first number: "
-);
-const num02 = +prompt("Enter the second number: ");
-const num03 = +prompt("Enter the third number: ");
-const answer = num01 * num02 * num03;
+// // const num01 = +prompt(
+// //   "Will the sign be + or - ? Pick three numbers. Enter the first number: "
+// // );
+// // const num02 = +prompt("Enter the second number: ");
+// // const num03 = +prompt("Enter the third number: ");
+// // const answer = num01 * num02 * num03;
 
-if (answer >= 0) {
-  console.log(`The sign is +`);
-} else if (answer < 0) {
-  console.log(`The sign is -`);
-}
+// // if (answer >= 0) {
+// //   console.log(`The sign is +`);
+// // } else if (answer < 0) {
+// //   console.log(`The sign is -`);
+// }
 
 //JS Assignment 04
+// const num1 = +prompt("Enter a number:");
+
+// if (num1 >= 50 && num1 <= 100) {
+//   console.log(`${num1} is withing the range of 50 to 100`);
+// } else {
+//   console.log(`${num1} is not within the range of 50 to 100`);
+// }
 
 //JS Assignment 05
 
